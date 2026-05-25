42bbecb977848cf35a5be405d7301fa97725887e
Roberto Ferranti <roberto.ferranti@gmail.com>
2026-05-25 11:06:24 +0200

    :ref: used bracket notation when accessing disabled to prevent ts warning in derived classes

--- src/parsed-element.mjs
+++ src/parsed-element.mjs
@@ -1,96 +1,96 @@
-import { Attributes, LightSlots } from "./dom.mjs";
-import { registry } from "./registry.mjs";
+    import { Attributes, LightSlots } from "./dom.mjs";
+    import { registry } from "./registry.mjs";
 
 
 
 
 
-class ParsedElement extends HTMLElement {
-    static BITS = {
-        enqueue: (el) => { },
-        SLOTS: false,
-        OBSERVED: [],
-        /** @type {Record<String, import("./registry.mjs").Mapper>} */
-        ATTR_TO_MAPPER: {},
-        TEMPLATES: {}
-    }
-    static get observedAttributes() {
-        return this.BITS.OBSERVED;
-    }
-    #parsed = false;
-    #reflecting = 0;
-    #bits() {
-        return /** @type {typeof ParsedElement} */(this.constructor).BITS;
-    }
-    unmarshal(attr, str) {
-        return this.#bits().ATTR_TO_MAPPER[attr].unmarshal(str, attr, this);
-    }
-    marshal(attr, value) {
-        return this.#bits().ATTR_TO_MAPPER[attr].marshal(value, attr, this);
-    }
-    template(name) {
-        const { modules, data } = registry.context();
-        let t = this.#bits().TEMPLATES[name ?? 'default'].withData(data).withModules(modules);
-        for (let k of ['l10n', 'config']) {
-            const v = this.constructor[k];
-            if (v) {
-                t = t.withOverlay({ [k]: v });
+    class ParsedElement extends HTMLElement {
+        static BITS = {
+            enqueue: (el) => { },
+            SLOTS: false,
+            OBSERVED: [],
+            /** @type {Record<string, import("./registry.mjs").Mapper>} */
+            ATTR_TO_MAPPER: {},
+            TEMPLATES: {}
+        }
+        static get observedAttributes() {
+            return this.BITS.OBSERVED;
+        }
+        #parsed = false;
+        #reflecting = 0;
+        #bits() {
+            return /** @type {typeof ParsedElement} */(this.constructor).BITS;
+        }
+        unmarshal(attr, str) {
+            return this.#bits().ATTR_TO_MAPPER[attr].unmarshal(str, attr, this);
+        }
+        marshal(attr, value) {
+            return this.#bits().ATTR_TO_MAPPER[attr].marshal(value, attr, this);
+        }
+        template(name) {
+            const { modules, data } = registry.context();
+            let t = this.#bits().TEMPLATES[name ?? 'default'].withData(data).withModules(modules);
+            for (let k of ['l10n', 'config']) {
+                const v = this.constructor[k];
+                if (v) {
+                    t = t.withOverlay({ [k]: v });
+                }
+            }
+            return t;
+        }
+        connectedCallback() {
+            if (this.#parsed) {
+                return;
+            }
+            this.#bits().enqueue(this);
+        }
+        attributeChangedCallback(attr, oldValue, newValue) {
+            if (!this.#parsed || oldValue === newValue) {
+                return;
+            }
+            if (this.#reflecting > 0) {
+                return;
+            }
+            this[attr] = this.unmarshal(attr, newValue);
+        }
+        #disabledBeforeParsed = null;
+        formDisabledCallback(disabled) {
+            if (!this.#parsed) {
+                this.#disabledBeforeParsed = disabled;
+                return;
+            }
+            this['disabled'] = disabled;
+        }
+        async upgrade() {
+            if (this.#parsed) {
+                return;
+            }
+            this.#parsed = true;
+            const slots = this.#bits().SLOTS ? LightSlots.from(this) : undefined;
+            const observed = Object.fromEntries(this.#bits().OBSERVED.map(attribute => [attribute, this.unmarshal(attribute, this.getAttribute(attribute))]));
+            const disabled = this.#disabledBeforeParsed ?? false
+            await this.render({ slots, observed, disabled });
+        }
+        render(c) {
+        }
+        reflect(fn) {
+            ++this.#reflecting;
+            try {
+                fn();
+            } finally {
+                --this.#reflecting;
             }
         }
-        return t;
+        reflectTo(attr, value) {
+            ++this.#reflecting;
+            try {
+                Attributes.set(this, attr, this.marshal(attr, value));
+            } finally {
+                --this.#reflecting;
+            }
+        }    
     }
-    connectedCallback() {
-        if (this.#parsed) {
-            return;
-        }
-        this.#bits().enqueue(this);
-    }
-    attributeChangedCallback(attr, oldValue, newValue) {
-        if (!this.#parsed || oldValue === newValue) {
-            return;
-        }
-        if (this.#reflecting > 0) {
-            return;
-        }
-        this[attr] = this.unmarshal(attr, newValue);
-    }
-    #disabledBeforeParsed = null;
-    formDisabledCallback(disabled) {
-        if (!this.#parsed) {
-            this.#disabledBeforeParsed = disabled;
-            return;
-        }
-        this.disabled = disabled;
-    }
-    async upgrade() {
-        if (this.#parsed) {
-            return;
-        }
-        this.#parsed = true;
-        const slots = this.#bits().SLOTS ? LightSlots.from(this) : undefined;
-        const observed = Object.fromEntries(this.#bits().OBSERVED.map(attribute => [attribute, this.unmarshal(attribute, this.getAttribute(attribute))]));
-        const disabled = this.#disabledBeforeParsed ?? false
-        await this.render({ slots, observed, disabled });
-    }
-    render(c) {
-    }
-    reflect(fn) {
-        ++this.#reflecting;
-        try {
-            fn();
-        } finally {
-            --this.#reflecting;
-        }
-    }
-    reflectTo(attr, value) {
-        ++this.#reflecting;
-        try {
-            Attributes.set(this, attr, this.marshal(attr, value));
-        } finally {
-            --this.#reflecting;
-        }
-    }    
-}
 
 
-export { ParsedElement };
+    export { ParsedElement };
